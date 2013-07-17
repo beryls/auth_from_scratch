df6881223bd116cea15a79c56b78b15c47a1e683
Beryl <beryl.schragger@gmail.com>
2013-07-17 13:45:01 -0400

    Add user authentication functionality.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,12 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
+  # POST
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to root_url, :notice => "Signed up!"
+      redirect_to root_url, notice: "Signed up!"
     else
       render "new"
     end
